32dbb9ef0a889fbd2485cc2fa9722c325b99598a
Vaishnavipatil3117 <119510001+Vaishnavipatil3117@users.noreply.github.com>
2024-02-23 17:28:30 +0530

    Update divisible.js

--- divisible.js
+++ divisible.js
@@ -2,5 +2,5 @@ const isDivisible = (number) => {
     return number % 10 === 0;
 };
 
-console.log(isDivisible(30));  
-console.log(isDivisible(17));  
+console.log(isDivisible(90));  
+console.log(isDivisible(87));  
